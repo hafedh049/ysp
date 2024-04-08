4c83dafa15d7b3ea0d4ff964d10c1fc15360a876
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-04-08 11:28:50 +0100

    Updates

---
+++ lib/views/holder.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Holder extends StatefulWidget {
+  const Holder({super.key});
+
+  @override
+  State<Holder> createState() => _HolderState();
+}
+
+class _HolderState extends State<Holder> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
